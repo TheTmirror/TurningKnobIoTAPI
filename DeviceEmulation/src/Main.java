8b9876937af22ef905c900f9d6033bf2420050d0
Tristan Seidl <tristan.gamer@web.de>
2019-03-09 19:15:43 +0100

    NEW INITIAL COMMIT!!!!

--- DeviceEmulation/src/Main.java
+++
@@ -1,9 +0,0 @@
-
-public class Main {
-
-	public static void main(String[] args) {
-		UpnpManager manager = new UpnpManager();
-		manager.start();
-	}
-
-}
--- REST/src/main/java/org/tris/internal/Constants.java
+++
@@ -1,7 +0,0 @@
-package org.tris.internal;
-
-public class Constants {
-
-	public static final String NEW_MOTION_EVENT = "newMotionEvent";
-	
-}
